9680a75c48f9dfeb198eb8c59b3400f6831aef2a
dennisYun12 <hello.dennis0102@gmail.com>
2022-08-18 15:52:57 +0300

    Custom themes trial

--- social-app-mui/src/App.js
+++ social-app-mui/src/App.js
@@ -1,9 +1,31 @@
+import * as React from "react";
+import { Button, ThemeProvider, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Add } from "@mui/icons-material";
+import { styled } from '@mui/material';
+import { theme } from './theme';
+
 function App() {
+
+  const CustomButton = styled(Button)(({theme})=> ({
+    backgroundColor: "#FF00FF",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#808000",
+      color: "black",
+    }
+  }));
+
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div>
+        <Button startIcon={<SettingsIcon/>} variant="contained" color="primary">Primary</Button>
+        <Button variant="contained" color="secondary">secondary</Button>
+        <Button variant="contained" color="error">error</Button>
+        <CustomButton>Custom Button</CustomButton>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ social-app-mui/src/theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#008000",
+            light: "#00FF00"
+        },
+        secondary: {
+            main: "#800080",
+        },
+        error: {
+            main: "#FF0000"
+        },
+        otherColor: {
+            main: "#A52A2A"
+        }
+    }
+})
