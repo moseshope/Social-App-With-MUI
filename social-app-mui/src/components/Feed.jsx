e377e36c3d3a7bbfd5e9a8269ece9be50d6f088b
dennisYun12 <hello.dennis0102@gmail.com>
2022-08-19 16:46:25 +0300

    Feed structure design achieved, Posts created

--- social-app-mui/src/components/Feed.jsx
+++ social-app-mui/src/components/Feed.jsx
@@ -1,9 +1,14 @@
+//import { Favorite, FavoriteBorder, MoreVert, Share } from '@mui/icons-material';
 import { Box } from '@mui/material';
 import React from 'react'
+import Posts from './Posts';
 
 const Feed = () => {
   return (
-    <Box bgcolor='pink' flex={4} p={2}>Feed</Box>
+    <Box flex={4} p={2}>
+      <Posts/>
+      <Posts/>
+    </Box>
   )
 }
 
