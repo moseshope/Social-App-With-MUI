8b278baebf0b956b27c7798bd95be56b5cee23cf
dennisYun12 <hello.dennis0102@gmail.com>
2022-08-19 16:53:08 +0300

    Added box position to fixed on side and RightBar

--- social-app-mui/src/components/RightBar.jsx
+++ social-app-mui/src/components/RightBar.jsx
@@ -1,10 +1,19 @@
-import { Box } from '@mui/material';
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 const RightBar = () => {
   return (
-    <Box bgcolor="lightcoral" flex={2} p={2} sx={{display: {xs: "none", sm: "block"}}}>RightBar</Box>
-  )
-}
+    <Box
+      bgcolor="lightcoral"
+      flex={2}
+      p={2}
+      sx={{ display: { xs: "none", sm: "block" } }}
+    >
+      <Box position="fixed">
+        RightBar
+      </Box>
+    </Box>
+  );
+};
 
 export default RightBar;
--- social-app-mui/src/components/SideBar.jsx
+++ social-app-mui/src/components/SideBar.jsx
@@ -5,6 +5,7 @@ import React from "react";
 const SideBar = () => {
   return (
     <Box flex={1} p={2} sx={{display: { xs: "none", sm: "block"}}}>
+      <Box position="fixed">
       <List>
       <ListItem disablePadding>
       <ListItemButton component="a" href="#home">
@@ -71,6 +72,7 @@ const SideBar = () => {
       </ListItemButton>
       </ListItem>
       </List>
+      </Box>
     </Box>
   );
 };
