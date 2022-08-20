2da1b6f4e23955c9170284813d0e9f28677718ad
dennisYun12 <hello.dennis0102@gmail.com>
2022-08-20 23:17:50 +0300

    Updated RightBar

--- social-app-mui/src/components/RightBar.jsx
+++ social-app-mui/src/components/RightBar.jsx
@@ -1,16 +1,15 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const RightBar = () => {
   return (
     <Box
-      bgcolor="lightcoral"
       flex={2}
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
       <Box position="fixed">
-        RightBar
+        <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
       </Box>
     </Box>
   );
