38dacebd03b8b8d607b15497f42a2596ee0f3c16
dennisYun12 <hello.dennis0102@gmail.com>
2022-09-04 23:57:55 +0300

    Added the account page

---
+++ social-app-mui/src/Pages/Account/Account.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Account = () => {
+  return (
+    <React.Fragment>
+      <Box>
+        Account
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default Account;
