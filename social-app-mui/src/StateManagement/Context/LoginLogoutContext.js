2f68adb037a1e8fa83e67f4b7f99be1f2697d920
dennisYun12 <hello.dennis0102@gmail.com>
2022-09-09 23:46:15 +0300

    Passed Login Logout State to index.js successfully

--- social-app-mui/src/StateManagement/Context/LoginLogoutContext.js
+++ social-app-mui/src/StateManagement/Context/LoginLogoutContext.js
@@ -4,13 +4,13 @@ const IsLoggedIn = createContext();
 
 const IsLoggedInContext = ({children}) => {
     const [Login, setLogin] = useState(false)
-    const [Logout, setLogout] = useState(true)
+    const [Logout, setLogout] = useState(false)
 
     useEffect(()=> {
         if (Logout === true) {
             alert("Please SignIn to use that function")
         }
-        else if (Logout === false) {
+        else if (Login === false) {
             alert("You can now do this function")
         }
     })
--- social-app-mui/src/index.js
+++ social-app-mui/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import IsLoggedInContext from "./StateManagement/Context/LoginLogoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <IsLoggedInContext>
+        <App />
+      </IsLoggedInContext>
     </BrowserRouter>
   </React.StrictMode>
 );
